1ce309b1eace1f0614dfb8dd0181e75d69c1e786
Drinkaiii <kai410705@gmail.com>
2024-09-13 11:11:40 +0800

    [init]

---
+++ src/main/java/com/oneswap/OneswapCoreApplication.java
@@ -0,0 +1,13 @@
+package com.oneswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneswapCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneswapCoreApplication.class, args);
+	}
+
+}
